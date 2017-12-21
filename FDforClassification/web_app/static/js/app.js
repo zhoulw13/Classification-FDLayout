f3b50bfd5b14786b350c09ebc057045a22637f56
Jim Ranor <jimranor@outlook.com>
2017-12-21 18:52:56 +0800

    no comments

--- FDforClassification/web_app/static/js/app.js
+++ FDforClassification/web_app/static/js/app.js
@@ -43,8 +43,8 @@ var ViewerApp;
             //_this.dataInfoPanel.updatePanel4OverAll();
             // if user selection is class 5
             //_this.dataInfoPanel.updatePanel4Class(5);
-            // if user selection is sample 15.png
-            _this.dataInfoPanel.updatePanel4Sample("15.png");
+            // if user selection is sample 123.png
+            _this.dataInfoPanel.updatePanel4Sample("123.png");
         };
 
         // callback function to start training simulation from current iterationId
